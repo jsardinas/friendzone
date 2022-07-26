885222d67b25afeb2bb24d46be5b425ee9fb1651
Javier <javier.sardinas@gmail.com>
2022-07-26 13:54:19 -0500

    Add route to get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,17 +4,16 @@ const { User } = require('../models');
 module.exports = {
     // Get all students
     getUsers(req, res) {
-    //   User.find()
-    //     .then(async (users) => {
-    //       const userObj = {
-    //         users,
-    //       };
-    //       return res.json(userObj);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //       return res.status(500).json(err);
-    //     });
-        res.status(200).send('OK');
+        User.find()
+        .then(async (users) => {
+            const userObj = {
+                users,
+            };
+            return res.json(userObj);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
     },
 }
