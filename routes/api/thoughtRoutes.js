92137feaafd62b69ef5d3e5e3ba6ad97838f6ac3
Javier <javier.sardinas@gmail.com>
2022-07-27 00:17:16 -0500

    Add thought seed and route to get all the thoughts

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+//   getSingleThought,
+//   createThought,
+//   updateThought,
+//   deleteThought
+} = require('../../controllers/thoughtController');
+
+// /api/thoughs
+router.route('/').get(getThoughts);//.post(createThought);
+
+// /api/thoughs/:thoughId
+//router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+module.exports = router;
--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,7 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
 const userData = require('./userData.json');
+const thoughtData = require('./thoughtData.json');
 
 connection.on('error', (err) => err);
 
@@ -12,7 +13,18 @@ connection.once('open', async () => {
 
     await Thought.deleteMany({});
 
-    await User.collection.insertMany(userData);
+    const u = await User.collection.insertMany(userData);
+
+    for (t of thoughtData){
+        let idx = Math.floor(Math.random()*userData.length);
+        console.log(idx);
+        t['username'] = userData[idx].username;
+        t['userId'] = u.insertedIds[idx];
+    }
+
+    console.log(thoughtData);
+    const th = await Thought.collection.insertMany(thoughtData);
+    console.log(th);
 
     console.info('Seeding complete! 🌱');
     process.exit(0);
